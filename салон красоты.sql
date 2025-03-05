4f9456f50db55f259745f1a976b464bd9bc596c4
Анна <annastepankova41@gmail.com>
2025-03-05 15:39:18 +0700

    Update салон красоты.sql

--- салон красоты.sql
+++ салон красоты.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `appointments1`
 --
 
-DROP TABLE IF EXISTS `appointments1`;
+DROP TABLE IF EXISTS `appointmentsI1`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `appointments1` (
